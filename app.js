2e9663fb5dc947b210d83a2a8861541b3752321b
MihirKapile <mihirkapile@gmail.com>
2023-08-18 02:33:29 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import UserController from "./users/routes.js";
+import session from "express-session";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.DATA_DB || "mongodb://localhost:27017/project"
+);
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+  },
+};
+if (process.env.NODE_ENV && process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+  app.set("trust proxy", 1);
+}
+app.use(session(sessionOptions));
+app.use(express.json());
+
+UserController(app);
+app.listen(process.env.PORT || 4000);
